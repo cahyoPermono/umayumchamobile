1195cb6f00b640d4bca2ec7026fc1540e92f201a
Cahyo Permono <cahyo.adi@imaniprima.com>
2025-07-20 15:38:38 +0700

    `Removed InventoryController and ConsumableController from DashboardBinding`

--- lib/bindings/dashboard_binding.dart
+++ lib/bindings/dashboard_binding.dart
@@ -1,15 +1,11 @@
 import 'package:get/get.dart';
-import 'package:umayumcha/controllers/inventory_controller.dart';
-import 'package:umayumcha/controllers/consumable_controller.dart';
 import 'package:umayumcha/controllers/delivery_note_controller.dart';
 import 'package:umayumcha/controllers/consumable_transaction_log_controller.dart';
 
 class DashboardBinding extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<InventoryController>(() => InventoryController());
     Get.lazyPut<DeliveryNoteController>(() => DeliveryNoteController());
-    Get.lazyPut<ConsumableController>(() => ConsumableController());
     Get.lazyPut<ConsumableTransactionLogController>(
       () => ConsumableTransactionLogController(),
     );
